985722d4dedcc51efa791edfe73fa4baff7216be
parkjihwan <mankcom7708@gmail.com>
2025-05-20 22:49:43 +0900

    feat : Code Rabbit
    Code Rabbit 한국어로 적혀 나오도록 수정
    
    #17

--- src/main/java/com/example/memecommerceback/global/config/WebSecurityConfig.java
+++ src/main/java/com/example/memecommerceback/global/config/WebSecurityConfig.java
@@ -17,4 +17,18 @@ public class WebSecurityConfig {
         = SecurityContextHolder.getContext().getAuthentication();
     return authentication;
   }
+
+  // 쓸 곳 없는 메서드와 메서드 명을 한글로 적기
+  void 테스트2(){
+    // System.out.println -> 불필요한 구문 잡아내는지?
+    System.out.println("안녕? 코드 레빗");
+  }
+
+  // 해당 컴피그 파일에 필요 없는 메서드
+  public Authentication getAuthentication2(){
+    // 변수 선언을 할 필요가 없었지만, 하는 경우
+    Authentication authentication
+        = SecurityContextHolder.getContext().getAuthentication();
+    return authentication;
+  }
 }
