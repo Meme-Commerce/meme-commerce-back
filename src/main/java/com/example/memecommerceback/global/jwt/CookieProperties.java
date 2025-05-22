07dd6d50fb216c54ef3a365bbd2230327708dd12
parkjihwan <mankcom7708@gmail.com>
2025-05-22 17:18:41 +0900

    feat : Common
    불필요한 어노테이션 제거
    
    This closes #21

--- src/main/java/com/example/memecommerceback/global/jwt/CookieProperties.java
+++ src/main/java/com/example/memecommerceback/global/jwt/CookieProperties.java
@@ -2,16 +2,14 @@ package com.example.memecommerceback.global.jwt;
 
 import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.bind.ConstructorBinding;
 
 @Getter
-@ConstructorBinding
 @ConfigurationProperties(prefix = "cookie")
 public class CookieProperties {
-  private String domain;
-  private String sameSite;
-  private boolean httpOnly;
-  private boolean secure;
+  private final String domain;
+  private final String sameSite;
+  private final boolean httpOnly;
+  private final boolean secure;
 
   public CookieProperties(String domain, String sameSite, boolean httpOnly, boolean secure) {
     this.domain = domain;
@@ -19,7 +17,4 @@ public class CookieProperties {
     this.httpOnly = httpOnly;
     this.secure = secure;
   }
-
-  // application.yml은 런타임 때 바뀌지 않는다.
-  // application.yml에 아무리 setDomain을 해도 똑같다.
 }
--- src/main/java/com/example/memecommerceback/global/jwt/JwtTokenDto.java
+++ src/main/java/com/example/memecommerceback/global/jwt/JwtTokenDto.java
@@ -1,12 +1,10 @@
 package com.example.memecommerceback.global.jwt;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
-@AllArgsConstructor
 public class JwtTokenDto {
   private final String accessToken;
   private final String refreshToken;
