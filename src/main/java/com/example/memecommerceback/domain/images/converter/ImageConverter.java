07ee67f396df1037cccad3561fe330e61039e376
parkjihwan <mankcom7708@gmail.com>
2025-05-25 02:50:03 +0900

    fix : Product
    상품 수정 API 구현

--- src/main/java/com/example/memecommerceback/domain/images/converter/ImageConverter.java
+++ src/main/java/com/example/memecommerceback/domain/images/converter/ImageConverter.java
@@ -24,32 +24,10 @@ public class ImageConverter {
         .build();
   }
 
-  public static Image toEntity(
-      S3ResponseDto s3ResponseDto, User user, Product product){
-    return Image.builder()
-        .url(s3ResponseDto.getUrl())
-        .size(s3ResponseDto.getSize())
-        .width(s3ResponseDto.getWidth())
-        .height(s3ResponseDto.getHeight())
-        .extension(s3ResponseDto.getExtension())
-        .fileName(s3ResponseDto.getFileName())
-        .originalName(s3ResponseDto.getOriginalName())
-        .userId(user.getId())
-        .ownerNickname(user.getNickname())
-        .product(product)
-        .build();
-  }
-
   public static List<Image> toEntityList(
       List<S3ResponseDto> s3ResponseDtoList, User user){
     return s3ResponseDtoList.stream().map(
         s3ResponseDto -> ImageConverter.toEntity(s3ResponseDto, user)).toList();
-  }
-
-  public static List<Image> toEntityList(
-      List<S3ResponseDto> s3ResponseDtoList, User user, Product product){
-    return s3ResponseDtoList.stream().map(
-        s3ResponseDto -> ImageConverter.toEntity(s3ResponseDto, user, product)).toList();
   }
 
   public static List<ImageResponseDto> toResponseDtoList(List<Image> imageList){
